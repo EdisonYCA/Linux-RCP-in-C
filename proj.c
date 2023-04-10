8eef8ccb4438e6e43e612abb450957924000cdb9
Edison Caceres <edisonyca127@gmail.com>
2023-04-10 14:46:30 -0400

    Added headers needed for server socket creation

--- proj.c
+++ proj.c
@@ -2,3 +2,8 @@
 that can be shared between the client and the server.
 */
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
