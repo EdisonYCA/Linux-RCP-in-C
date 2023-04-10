ccf9146c264921a8882070744a2b86f1125740af
Riggie Lin <45950807+sunny13624@users.noreply.github.com>
2023-04-10 19:15:51 -0400

    Updated proj.c

--- proj.c
+++ proj.c
@@ -2,12 +2,19 @@
 that can be shared between the client and the server.
 */
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+
 
 extern int errno;
 
